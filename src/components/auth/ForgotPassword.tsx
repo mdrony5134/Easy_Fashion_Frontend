0c978134a64acfa58d1586af8c66517a1e370439
mdrony5134 <rony830388@gmail.com>
2026-08-07 15:32:39 +0600

    solved build error

--- src/components/auth/ForgotPassword.tsx
+++ src/components/auth/ForgotPassword.tsx
@@ -1,103 +1,103 @@
-"use client";
+// "use client";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { useResetPasswordSentOtpMutation } from "@/redux/api/userApi";
-import { Loader2 } from "lucide-react";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import type React from "react";
-import { useState } from "react";
-import { toast } from "sonner";
+// import { Button } from "@/components/ui/button";
+// import { Input } from "@/components/ui/input";
+// import { Label } from "@/components/ui/label";
+// import { useResetPasswordSentOtpMutation } from "@/redux/api/userApi";
+// import { Loader2 } from "lucide-react";
+// import Link from "next/link";
+// import { useRouter } from "next/navigation";
+// import type React from "react";
+// import { useState } from "react";
+// import { toast } from "sonner";
 
-export default function ForgotPassword() {
-  const [email, setEmail] = useState("");
+// export default function ForgotPassword() {
+//   const [email, setEmail] = useState("");
 
-  const [resetPasswordFn, { isLoading }] = useResetPasswordSentOtpMutation();
-  const router = useRouter();
+//   const [resetPasswordFn, { isLoading }] = useResetPasswordSentOtpMutation();
+//   const router = useRouter();
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const response = await resetPasswordFn({email: email}).unwrap();
+//   const handleSubmit = async (e: React.FormEvent) => {
+//     e.preventDefault();
+//     try {
+//       const response = await resetPasswordFn({email: email}).unwrap();
 
-      if (response?.success) {
-        toast.success("A verification code has been sent to your email.");
-        router.push(`/verify-otp?email=${email}`);
-      } else {
-        toast.error("Something went wrong. Please try again.");
-      }
-    } catch  {
-      toast.error("Failed to send verification code. Please check your email.");
-    }
-  };
+//       if (response?.success) {
+//         toast.success("A verification code has been sent to your email.");
+//         router.push(`/verify-otp?email=${email}`);
+//       } else {
+//         toast.error("Something went wrong. Please try again.");
+//       }
+//     } catch  {
+//       toast.error("Failed to send verification code. Please check your email.");
+//     }
+//   };
 
-  return (
-    <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-2xl w-full space-y-8 border border-[#ADADAD40] rounded-2xl p-8 shadow-sm ">
-        <div className="text-center">
-          <h1 className="text-3xl font-bold text-gray-900 mb-8">
-            Forgot password
-          </h1>
-        </div>
+//   return (
+//     <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+//       <div className="max-w-2xl w-full space-y-8 border border-[#ADADAD40] rounded-2xl p-8 shadow-sm ">
+//         <div className="text-center">
+//           <h1 className="text-3xl font-bold text-gray-900 mb-8">
+//             Forgot password
+//           </h1>
+//         </div>
 
-        <form onSubmit={handleSubmit} className="space-y-6">
-          <div>
-            <Label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700 mb-2"
-            >
-              Email Address
-            </Label>
-            <Input
-              id="email"
-              name="email"
-              type="email"
-              placeholder="Enter your email address"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full px-4 py-6 border border-gray-300 rounded-xl focus:ring-2 focus:ring-primary focus:border-primary"
-              required
-            />
-          </div>
+//         <form onSubmit={handleSubmit} className="space-y-6">
+//           <div>
+//             <Label
+//               htmlFor="email"
+//               className="block text-sm font-medium text-gray-700 mb-2"
+//             >
+//               Email Address
+//             </Label>
+//             <Input
+//               id="email"
+//               name="email"
+//               type="email"
+//               placeholder="Enter your email address"
+//               value={email}
+//               onChange={(e) => setEmail(e.target.value)}
+//               className="w-full px-4 py-6 border border-gray-300 rounded-xl focus:ring-2 focus:ring-primary focus:border-primary"
+//               required
+//             />
+//           </div>
 
-          <div className="text-left">
-            <span className="text-gray-600">Remember the password? </span>
-            <Link
-              href="/login"
-              className="text-primary hover:text-primary font-medium"
-            >
-              Sign in
-            </Link>
-          </div>
+//           <div className="text-left">
+//             <span className="text-gray-600">Remember the password? </span>
+//             <Link
+//               href="/login"
+//               className="text-primary hover:text-primary font-medium"
+//             >
+//               Sign in
+//             </Link>
+//           </div>
 
-          <Button
-            type="submit"
-            disabled={isLoading}
-            className="w-full bg-primary text-white font-medium py-6 px-4 rounded-[40px] transition-colors flex items-center justify-center gap-2"
-          >
-            {isLoading ? (
-              <>
-                <Loader2 className="animate-spin h-5 w-5" />
-                Sending...
-              </>
-            ) : (
-              "Send Code"
-            )}
-          </Button>
+//           <Button
+//             type="submit"
+//             disabled={isLoading}
+//             className="w-full bg-primary text-white font-medium py-6 px-4 rounded-[40px] transition-colors flex items-center justify-center gap-2"
+//           >
+//             {isLoading ? (
+//               <>
+//                 <Loader2 className="animate-spin h-5 w-5" />
+//                 Sending...
+//               </>
+//             ) : (
+//               "Send Code"
+//             )}
+//           </Button>
 
-          <div className="text-center">
-            <span className="text-gray-600">Don’t have an account? </span>
-            <Link
-              href="/signup"
-              className="text-primary hover:text-primary font-medium"
-            >
-              Sign up
-            </Link>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
-}
+//           <div className="text-center">
+//             <span className="text-gray-600">Don’t have an account? </span>
+//             <Link
+//               href="/signup"
+//               className="text-primary hover:text-primary font-medium"
+//             >
+//               Sign up
+//             </Link>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
--- src/components/auth/VerifyOtp.tsx
+++ src/components/auth/VerifyOtp.tsx
@@ -1,124 +1,124 @@
-"use client";
+// "use client";
 
-import { Button } from "@/components/ui/button";
-import { useResetPasswordVerifyOtpMutation } from "@/redux/api/userApi";
-import Cookies from "js-cookie";
-import { Loader2 } from "lucide-react";
-import { useRouter, useSearchParams } from "next/navigation";
-import type React from "react";
-import { useRef, useState } from "react";
-import { toast } from "sonner";
+// import { Button } from "@/components/ui/button";
+// import { useResetPasswordVerifyOtpMutation } from "@/redux/api/userApi";
+// import Cookies from "js-cookie";
+// import { Loader2 } from "lucide-react";
+// import { useRouter, useSearchParams } from "next/navigation";
+// import type React from "react";
+// import { useRef, useState } from "react";
+// import { toast } from "sonner";
 
-export default function VerifyOTPPage() {
-  const [otp, setOtp] = useState(["", "", "", ""]);
-  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
-  const params = useSearchParams();
-  const email = params.get("email");
-  const router = useRouter();
+// export default function VerifyOTPPage() {
+//   const [otp, setOtp] = useState(["", "", "", ""]);
+//   const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
+//   const params = useSearchParams();
+//   const email = params.get("email");
+//   const router = useRouter();
 
-  const [verifyOtpFn, { isLoading }] = useResetPasswordVerifyOtpMutation();
+//   const [verifyOtpFn, { isLoading }] = useResetPasswordVerifyOtpMutation();
 
-  const handleInputChange = (index: number, value: string) => {
-    if (value.length > 1) return; // Prevent multiple characters
+//   const handleInputChange = (index: number, value: string) => {
+//     if (value.length > 1) return; // Prevent multiple characters
 
-    const newOtp = [...otp];
-    newOtp[index] = value;
-    setOtp(newOtp);
+//     const newOtp = [...otp];
+//     newOtp[index] = value;
+//     setOtp(newOtp);
 
-    // Auto-focus next input
-    if (value && index < 3) {
-      inputRefs.current[index + 1]?.focus();
-    }
-  };
+//     // Auto-focus next input
+//     if (value && index < 3) {
+//       inputRefs.current[index + 1]?.focus();
+//     }
+//   };
 
-  const handleKeyDown = (
-    index: number,
-    e: React.KeyboardEvent<HTMLInputElement>
-  ) => {
-    // Handle backspace to move to previous input
-    if (e.key === "Backspace" && !otp[index] && index > 0) {
-      inputRefs.current[index - 1]?.focus();
-    }
-  };
+//   const handleKeyDown = (
+//     index: number,
+//     e: React.KeyboardEvent<HTMLInputElement>
+//   ) => {
+//     // Handle backspace to move to previous input
+//     if (e.key === "Backspace" && !otp[index] && index > 0) {
+//       inputRefs.current[index - 1]?.focus();
+//     }
+//   };
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    const otpCode = otp.join("");
+//   const handleSubmit = async (e: React.FormEvent) => {
+//     e.preventDefault();
+//     const otpCode = otp.join("");
 
-    if (!email) {
-      toast.error("Email is missing. Please try again.");
-      return;
-    }
+//     if (!email) {
+//       toast.error("Email is missing. Please try again.");
+//       return;
+//     }
 
-    try {
-      const response = await verifyOtpFn({ email, otp: otpCode }).unwrap();
+//     try {
+//       const response = await verifyOtpFn({ email, otp: otpCode }).unwrap();
 
-      if (response?.success) {
-        toast.success(
-          "OTP verified successfully. You can now reset your password."
-        );
-        router.push(`/reset-password`);
-        Cookies.set("forgetToken", response?.result?.forgetToken);
-      } else {
-        toast.error("Invalid OTP. Please try again.");
-      }
-    } catch  {
-      toast.error("Failed to verify OTP. Please try again later.");
-    }
-  };
+//       if (response?.success) {
+//         toast.success(
+//           "OTP verified successfully. You can now reset your password."
+//         );
+//         router.push(`/reset-password`);
+//         Cookies.set("forgetToken", response?.result?.forgetToken);
+//       } else {
+//         toast.error("Invalid OTP. Please try again.");
+//       }
+//     } catch  {
+//       toast.error("Failed to verify OTP. Please try again later.");
+//     }
+//   };
 
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-2xl w-full space-y-8 border border-[#ADADAD40] rounded-2xl p-8 shadow-sm ">
-        <div className="text-center">
-          <h1 className="text-3xl font-bold text-gray-900 mb-4">
-            Check your Email
-          </h1>
-          <p className="text-gray-600 mb-8">
-            We sent a four digit code to your email address
-          </p>
-        </div>
+//   return (
+//     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+//       <div className="max-w-2xl w-full space-y-8 border border-[#ADADAD40] rounded-2xl p-8 shadow-sm ">
+//         <div className="text-center">
+//           <h1 className="text-3xl font-bold text-gray-900 mb-4">
+//             Check your Email
+//           </h1>
+//           <p className="text-gray-600 mb-8">
+//             We sent a four digit code to your email address
+//           </p>
+//         </div>
 
-        <form onSubmit={handleSubmit} className="space-y-8">
-          <div className="flex justify-center space-x-4">
-            {otp.map((digit, index) => (
-              <input
-                key={index}
-                ref={(el) => {
-                  inputRefs.current[index] = el;
-                }}
-                type="text"
-                maxLength={1}
-                value={digit}
-                onChange={(e) => handleInputChange(index, e.target.value)}
-                onKeyDown={(e) => handleKeyDown(index, e)}
-                className="w-16 h-16 text-center text-xl font-semibold border border-gray-300 rounded-lg focus:ring-2 focus:ring-primary focus:border-primary outline-none"
-              />
-            ))}
-          </div>
+//         <form onSubmit={handleSubmit} className="space-y-8">
+//           <div className="flex justify-center space-x-4">
+//             {otp.map((digit, index) => (
+//               <input
+//                 key={index}
+//                 ref={(el) => {
+//                   inputRefs.current[index] = el;
+//                 }}
+//                 type="text"
+//                 maxLength={1}
+//                 value={digit}
+//                 onChange={(e) => handleInputChange(index, e.target.value)}
+//                 onKeyDown={(e) => handleKeyDown(index, e)}
+//                 className="w-16 h-16 text-center text-xl font-semibold border border-gray-300 rounded-lg focus:ring-2 focus:ring-primary focus:border-primary outline-none"
+//               />
+//             ))}
+//           </div>
 
-          <Button
-            type="submit"
-            disabled={isLoading}
-            className="w-full bg-primary hover:bg-orange-600 text-white font-medium py-6 px-4 rounded-[40px] transition-colors flex items-center justify-center gap-2"
-          >
-            {isLoading ? (
-              <>
-                <Loader2 className="animate-spin h-5 w-5" />
-                Verifying...
-              </>
-            ) : (
-              "Submit"
-            )}
-          </Button>
+//           <Button
+//             type="submit"
+//             disabled={isLoading}
+//             className="w-full bg-primary hover:bg-orange-600 text-white font-medium py-6 px-4 rounded-[40px] transition-colors flex items-center justify-center gap-2"
+//           >
+//             {isLoading ? (
+//               <>
+//                 <Loader2 className="animate-spin h-5 w-5" />
+//                 Verifying...
+//               </>
+//             ) : (
+//               "Submit"
+//             )}
+//           </Button>
 
-          <div className="text-center">
-            <p className="text-gray-600">
-              Can&apos;t find the email? Check your spam folder.
-            </p>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
-}
+//           <div className="text-center">
+//             <p className="text-gray-600">
+//               Can&apos;t find the email? Check your spam folder.
+//             </p>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
